90bc4e23ec112920b84146326144184356864ec7
Krish Verma <dhurandhar0987@gmail.com>
2023-08-20 23:38:32 +0530

    Initial commit Log of all sensor names and vendors

---
+++ app/src/main/java/com/example/hardwaresensors/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.hardwaresensors
+
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.getSystemService
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sensorManager = getSystemService<SensorManager>()
+        if(sensorManager == null){
+            Toast.makeText(this, "Could not get sensors", Toast.LENGTH_SHORT)
+            finish()
+        }else{
+            val sensors = sensorManager.getSensorList(Sensor.TYPE_ALL)
+            sensors.forEach{
+                Log.d("HWSENS",""" 
+                    ${it.name} | ${it.stringType} | ${it.vendor}
+                """.trimIndent() )
+            }
+        }
+    }
+}
