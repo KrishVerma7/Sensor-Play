28b223fb4978980921d97efd72c993c3ba765079
Krish Verma <dhurandhar0987@gmail.com>
2023-08-22 22:49:30 +0530

    implemented - placing palm near screen shows value 5 , only when app is open

--- app/src/main/java/com/example/hardwaresensors/MainActivity.kt
+++ app/src/main/java/com/example/hardwaresensors/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.hardwaresensors
 
+import android.graphics.Color
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,22 +11,37 @@ import android.util.Log
 import android.widget.Toast
 import androidx.core.content.getSystemService
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    lateinit var sensorManager: SensorManager
+    lateinit var proxSensor: Sensor
+
+    //this function is used to detect accuracy based things like difference in brightness
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+    override fun onSensorChanged(event: SensorEvent?) {
+        Log.d("HWSENS", """
+                     onSensorChanged: ${event!!.values[0]}
+                 """.trimIndent())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sensorManager = getSystemService<SensorManager>()
-        if(sensorManager == null){
-            Toast.makeText(this, "Could not get sensors", Toast.LENGTH_SHORT)
-            finish()
-        }else{
-            val sensors = sensorManager.getSensorList(Sensor.TYPE_ALL)
-            sensors.forEach{
-                Log.d("HWSENS",""" 
-                    ${it.name} | ${it.stringType} | ${it.vendor}
-                """.trimIndent() )
-            }
-        }
+        sensorManager = getSystemService<SensorManager>()!!
+        proxSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)!!
     }
+
+    //lists the values of proximity sensor only when app is open
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, proxSensor,1000*1000)
+    }
+
+    //this pauses the sensor work when app is closed or screen is turned off
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
 }
