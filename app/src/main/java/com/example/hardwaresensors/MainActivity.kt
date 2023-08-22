a20c604767dc95b44f52f2736cbe3a29855fbc0f
Krish Verma <dhurandhar0987@gmail.com>
2023-08-22 23:34:29 +0530

    implemented - Accelerometer and showing output in logcat

--- app/src/main/java/com/example/hardwaresensors/MainActivity.kt
+++ app/src/main/java/com/example/hardwaresensors/MainActivity.kt
@@ -8,34 +8,53 @@ import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.core.content.getSystemService
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     lateinit var sensorManager: SensorManager
     lateinit var proxSensor: Sensor
+    lateinit var accelSensor: Sensor
+    lateinit var flProxIndicator: FrameLayout
+    lateinit var flAccelIndicator: FrameLayout
+
+    val colors = arrayOf(Color.RED , Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN)
 
     //this function is used to detect accuracy based things like difference in brightness
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
     override fun onSensorChanged(event: SensorEvent?) {
-        Log.d("HWSENS", """
-                     onSensorChanged: ${event!!.values[0]}
-                 """.trimIndent())
+        flProxIndicator= findViewById(R.id.flProxIndicator)
+        flAccelIndicator= findViewById(R.id.flProxIndicator)
+//        if(event!!.values[0]>0){
+//            flProxIndicator.setBackgroundColor(colors[Random.nextInt(5)])
+//        }
+        Log.d("HWSENS","""
+            ----
+            ax = ${event!!.values[0]}
+            ay = ${event!!.values[1]}
+            az = ${event!!.values[2]}
+            ----
+        """.trimIndent())
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         sensorManager = getSystemService<SensorManager>()!!
         proxSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)!!
+        accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)!!
+
     }
 
     //lists the values of proximity sensor only when app is open
     override fun onResume() {
         super.onResume()
-        sensorManager.registerListener(this, proxSensor,1000*1000)
+        sensorManager.registerListener(this, accelSensor,1000*1000)
     }
 
     //this pauses the sensor work when app is closed or screen is turned off
