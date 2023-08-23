a773c7ecbf671b7146ef39c6e7953714cc4dda57
Krish Verma <dhurandhar0987@gmail.com>
2023-08-23 11:53:10 +0530

    implemented - Accelerometer and proximity sensor together

--- app/src/main/java/com/example/hardwaresensors/MainActivity.kt
+++ app/src/main/java/com/example/hardwaresensors/MainActivity.kt
@@ -11,6 +11,7 @@ import android.util.Log
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.core.content.getSystemService
+import kotlin.math.roundToInt
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
@@ -21,46 +22,49 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     lateinit var flProxIndicator: FrameLayout
     lateinit var flAccelIndicator: FrameLayout
 
-    val colors = arrayOf(Color.RED , Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN)
+    val colors = arrayOf(Color.RED, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN, Color.YELLOW, Color.DKGRAY)
 
-    //this function is used to detect accuracy based things like difference in brightness
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
     override fun onSensorChanged(event: SensorEvent?) {
-        flProxIndicator= findViewById(R.id.flProxIndicator)
-        flAccelIndicator= findViewById(R.id.flProxIndicator)
-//        if(event!!.values[0]>0){
-//            flProxIndicator.setBackgroundColor(colors[Random.nextInt(5)])
-//        }
-        Log.d("HWSENS","""
-            ----
-            ax = ${event!!.values[0]}
-            ay = ${event!!.values[1]}
-            az = ${event!!.values[2]}
-            ----
-        """.trimIndent())
+        if (event != null) {
+            if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
+                if (event.values[0] > 0) {
+                    flProxIndicator.setBackgroundColor(colors[Random.nextInt(7)])
+                }
+            } else if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                val bgColor = Color.rgb(
+                    accel2color(event.values[0]),
+                    accel2color(event.values[1]),
+                    accel2color(event.values[2])
+                )
+                flAccelIndicator.setBackgroundColor(bgColor)
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        flProxIndicator = findViewById(R.id.flProxIndicator)
+        flAccelIndicator = findViewById(R.id.flAccelIndicator)
 
         sensorManager = getSystemService<SensorManager>()!!
         proxSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)!!
         accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)!!
-
     }
 
-    //lists the values of proximity sensor only when app is open
     override fun onResume() {
         super.onResume()
-        sensorManager.registerListener(this, accelSensor,1000*1000)
+        sensorManager.registerListener(this, proxSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
-    //this pauses the sensor work when app is closed or screen is turned off
     override fun onPause() {
         super.onPause()
         sensorManager.unregisterListener(this)
     }
 
+    private fun accel2color(accel: Float) = (((accel + 12) / 24) * 255).roundToInt()
 }
